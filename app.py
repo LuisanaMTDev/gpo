cbca56b3b47f794457c2e44263b8f29e149002f0
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2025-11-17 21:33:54 -0400

    feat(app.py): add code to save sessions file content to the db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import time
 from io import StringIO
 
 from dotenv import load_dotenv
@@ -54,8 +55,41 @@ def index_page():
 
 @app.post("/send-file")
 def get_csv_file():
-    sessions_file = request.files["sessions_file"]
+    # # NOTE: 604800 are the amount of seconds that 7 days has
+    SF_EXPIRED_DATE = 60
+    sessions_file = request.files.get("sessions_file")
+    if sessions_file is None:
+        # TODO: This should return a response with some html indicating the error
+        abort(400)
     with StringIO(sessions_file.stream.read().decode("UTF8"), newline=None) as stream:
+        sessions = get_unfiltered_sessions_as_list_of_dicts(stream)
+        _ = stream.seek(0)
+        days, hours = get_unique_days_and_hours(stream)
+        sessions_for_db = stream.getvalue()
+
+    timestamp = int(time.time())
+    setted = redis_client.set(f"sf-{timestamp}", sessions_for_db, ex=SF_EXPIRED_DATE)
+    if setted is False:
+        abort(500)
+
+    response = make_response(
+        render_template(
+            "sessions_fragments.html",
+            fragment="all_sessions",
+            sessions=sessions,
+            days=days,
+            hours=hours,
+        )
+    )
+    response.set_cookie(
+        key="sessions_file_key",
+        value=f"sf-{timestamp}",
+        max_age=SF_EXPIRED_DATE,
+        httponly=True,
+        # TODO: Add on production secure and samesite parameters.
+    )
+
+    return response
 
     return render_template(
         "sessions_fragments.html",
