efbd4b6594806c2bdd16b6debf0dbfea891c7223
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2025-11-18 00:04:50 -0400

    feat(sessions.py): change the filter_sessions function to filter based on the user input on the frontend

--- sessions.py
+++ sessions.py
@@ -7,29 +7,39 @@ MODALITY_ORDER = {"Virtual": 1, "Semipresencial": 2, "Presencial": 3}
 TYPE_ORDER = {"Laboratorio": 1, "Teoria": 2}
 
 
-def filter_sessions(csv_file: StringIO) -> tuple[list[dict[Hashable, str]], bool]:
-    df = pd.read_csv(csv_file)
+def filter_sessions(
+    csv_file: StringIO, days_to_exclude: list[str], hours_to_exclude: list[str]
+) -> tuple[list[dict[Hashable, str]], bool]:
+    original_sessions = pd.read_csv(csv_file)
 
     # NOTE: check is the subject is 70/30
-    is7030 = df["Tipo"].nunique() == 2
+    is7030 = original_sessions["Tipo"].nunique() == 2
 
-    filtered_sessions = df[
-        ~df["Horario"].str.contains("^(0[0-9]|1[0-2])", regex=True, na=False)
-        & ~df["Días"].str.contains("SA", na=False)
-        & ~df["Horario"].str.contains("2[1-4]$", regex=True, na=False)
+    filtered_sessions = original_sessions[
+        ~original_sessions["Días"].isin(days_to_exclude)
     ]
 
-    nrcs_duplicated = df[df.duplicated(subset=["NRC"], keep=False)]["NRC"].unique()
+    filtered_sessions = filtered_sessions[
+        ~filtered_sessions["Horario"].isin(hours_to_exclude)
+    ]
+
+    nrcs_duplicated = original_sessions[
+        original_sessions.duplicated(subset=["NRC"], keep=False)
+    ]["NRC"].unique()
 
     # NOTE:
     # set(df["NRC"]) - set(filtered_sessions["NRC"])
     # return the df's NRCs that aren't on filtered_sessions
-    nrcs_totally_exclured = set(df["NRC"]) - set(filtered_sessions["NRC"])
+    nrcs_totally_excluded = set(original_sessions["NRC"]) - set(
+        filtered_sessions["NRC"]
+    )
 
-    nrcs_partially_exclured = set(nrcs_duplicated) - nrcs_totally_exclured
+    nrcs_duplicated_and_partially_excluded = (
+        set(nrcs_duplicated) - nrcs_totally_excluded
+    )
 
     filtered_sessions = filtered_sessions[
-        ~filtered_sessions["NRC"].isin(nrcs_partially_exclured)
+        ~filtered_sessions["NRC"].isin(nrcs_duplicated_and_partially_excluded)
     ]
 
     filtered_sessions = filtered_sessions.sort_values(
